76d4d5f500983c1b3a51f6380ad8f66548af1e55
Adriano Souza <adrianosg028@gmail.com>
2022-07-13 23:38:36 -0300

    adicao dos codigos que implementam o padrao strategy

--- vendinha-php/src/model/dto/DinheiroDTO.php
+++ vendinha-php/src/model/dto/DinheiroDTO.php
@@ -1,10 +1,10 @@
 <?php
 
+//padrao strategy: adriano souza - 219.083.118
+
 declare(strict_types=1);
 
 namespace dto;
-
-// Luana Victória Gonçalves Vidal Camargo Leão - 219083082
 
 Class DinheiroDTO{
 
